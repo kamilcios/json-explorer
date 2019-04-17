1fed94f1c3401e75312f2592eb521f5ce3b2a974
Kamil <kamil.cios@gmail.com>
2019-04-17 21:51:23 +0200

    JSON Explorer initial commit

---
+++ js/app.js
@@ -0,0 +1,129 @@
+// json explorer
+
+const context = {
+  engine: 'jsonpath',
+  engines: {
+    jsonpath: function (query, json) {
+      if (!query.trim()) return json;
+
+      return jsonpath.query(json, query);
+    },
+    xpath: function (query, json) {
+      if (!query.trim()) return json;
+
+      return defiant.search(json, query);
+    }
+  },
+  json: {},
+  query: '',
+  ticker: null,
+  interval: 300,
+  result: {}
+};
+
+const template = `
+<div class="container">
+    <div class="header">
+        <select name="engine">
+            <option value="jsonpath">JSONPath</option>
+            <option value="xpath">XPath</option>
+        </select>
+        <input name="query" type="text" placeholder="Query">
+    </div>
+
+    <div class="code"><pre></pre></div>
+</div>`;
+
+function initialize () {
+  let pre = document.querySelector('body > pre');
+  if (!pre) return false;
+
+  try {
+    context.json = window.json = JSON.parse(pre.innerText);
+  } catch (e) {
+    return false;
+  }
+
+  bootstrap();
+  highlight(context.json);
+}
+
+function bootstrap() {
+  let pre = document.querySelector('body > pre');
+  pre.style.display = 'none';
+
+  let explorer = document.createElement('div');
+  explorer.id = 'json-explorer';
+  explorer.innerHTML = template.trim();
+
+  pre.after(explorer);
+
+  document.querySelector('#json-explorer select[name=engine]').addEventListener('change', function () {
+    if (!context.engines.hasOwnProperty(this.value)) return;
+
+    context.engine = this.value
+    search(context.query, context.json);
+  });
+
+  let changeHandler = function (event) {
+    if (event.target.value.trim() === context.query.trim()) return;
+
+    context.query = event.target.value;
+
+    clearTimeout(context.ticker);
+    context.ticker = setTimeout(function () { search(context.query, context.json); }, context.interval);
+  }
+
+  document.querySelector('#json-explorer input[name=query]').addEventListener('keyup', changeHandler);
+  document.querySelector('#json-explorer input[name=query]').addEventListener('change', changeHandler);
+}
+
+function _highlight(json) {
+  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+    var cls = 'number';
+    if (/^"/.test(match)) {
+      if (/:$/.test(match)) {
+        cls = 'key';
+      } else {
+        cls = 'string';
+      }
+    } else if (/true|false/.test(match)) {
+      cls = 'boolean';
+    } else if (/null/.test(match)) {
+      cls = 'null';
+    }
+    return '<span class="' + cls + '">' + match + '</span>';
+  });
+}
+
+function search(query = context.query, json = context.json) {
+  try {
+    let result = context.result = context.engines[context.engine](query, json);
+    success();
+    highlight(result);
+  } catch (e) {
+    error();
+  }
+}
+
+function highlight(json) {
+  let string = _highlight(JSON.stringify(json, null, 2));
+
+  print(string);
+}
+
+function print(input) {
+  let pre = document.querySelector('#json-explorer .code > pre');
+  pre.innerHTML = input;
+}
+
+function success() {
+  document.querySelector('#json-explorer .header').classList.remove('error');
+}
+
+function error() {
+  document.querySelector('#json-explorer .header').classList.add('error');
+}
+
+initialize();
